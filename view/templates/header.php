103e9e7f35fcc61643cacdd17219f6c2a360d5b2
Dennis-PC\dennis <99028292@mydavinci.nl>
2017-06-06 23:40:30 +0200

    Title aangepast naar PHP Kalender.

--- view/templates/header.php
+++ view/templates/header.php
@@ -4,7 +4,7 @@
 
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Studenten app</title>
+	<title>PHP Kalender</title>
 	<link rel="stylesheet" type="text/css" href="/Framework/css/main.css">
 </head>
 <body>
